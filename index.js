7a2f009d8be67126834c74927e76bf42a2424130
Sean Leather <sean.leather@gmail.com>
2020-06-15 10:48:17 +0200

    Fix order of plugins, add retext-spell-file

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 'use strict'
 
 exports.plugins = [
+  // This is first.
+  require('retext-english'),
   // Keep these sorted alphabetically.
-  [require('retext-spell'), {dictionary: require('dictionary-en')}],
   require('retext-contractions'),
   require('retext-diacritics'),
+  require('retext-emoji'),
   require('retext-indefinite-article'),
   require('retext-quotes'),
   require('retext-redundant-acronyms'),
   require('retext-repeated-words'),
   require('retext-sentence-spacing'),
+  require('retext-syntax-urls'),
+  // This is last.
+  [require('retext-spell-file'), {dictionary: require('dictionary-en')}],
 ]
